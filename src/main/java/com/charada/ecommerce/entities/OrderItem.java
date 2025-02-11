08862811f81979ca01265b158e5acef38be0eca0
Charles65-ops <charlessasmgunlima68@gmail.com>
2025-02-11 20:53:42 -0300

    make entities

--- src/main/java/com/charada/ecommerce/entities/OrderItem.java
+++ src/main/java/com/charada/ecommerce/entities/OrderItem.java
@@ -1,6 +1,17 @@
 package com.charada.ecommerce.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tb_order_item")
 public class OrderItem {
+    @EmbeddedId
+    private OrderItemPK id = new OrderItemPK();
+
+    private int quantity;
+    private double price;
 
 
 }
--- src/main/java/com/charada/ecommerce/entities/OrderItemPK.java
+++ src/main/java/com/charada/ecommerce/entities/OrderItemPK.java
@@ -1,8 +1,20 @@
 package com.charada.ecommerce.entities;
 
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
 
+@Data
 @Embeddable
 public class OrderItemPK {
     // Add fields for primary key
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
 }
